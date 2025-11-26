75b58d0ba7a7f03b847602c6d578a3f2fb6a6190
AnaSanches7 <ana.sanches.freitas@escola.pr.gov.br>
2025-11-26 09:53:44 -0300

    ewfjigeh

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,53 @@
-import { useState } from "react";
+import { useState } from "react"
+
+function App() {
+  const [task, setTask] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description: "Estudar programação para se tornar um desenvolvedor full stack",
+      isCompleted: false
+    },
+    {
+      id: 2,
+      title: "Estudar inglês",
+      description: "Estudar inglês para se tornar fluente",
+      isCompleted: false
+    },
+    {
+      id: 3,
+      title: "Estudar matemática",
+      description: "Estudar matemática para desenvolver minha lógica",
+      isCompleted: false
+    }
+  ]);
+
+  return (
+    <h1>Gerenciando tarefas</h1>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { useState } from "react";
+
 function App() {
   const [message, setMessage] = useState('Olá, mundo');
 
@@ -10,5 +59,5 @@ function App() {
   );
 }
 
-export default App
+export default App */
 
